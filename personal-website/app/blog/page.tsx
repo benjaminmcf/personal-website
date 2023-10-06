e1f62960db2bbf09ca8d2e3d267a3ff0747654bd
Benjamin McFadden <mcfaddenben7@gmail.com>
2023-10-06 11:32:15 +0800

    minor change

--- personal-website/app/blog/page.tsx
+++ personal-website/app/blog/page.tsx
@@ -5,6 +5,8 @@ export default function blog() {
     <Navbar />
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>Benjamin McFadden - Blog</h1>
+        <h2>Blog posts</h2>
+        <h2>Coming soon</h2>
       </main>
     </>
       
